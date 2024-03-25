fe12f2fbe460cb2a6dd43a50fd8cf5a7ddb03f9e
Jeff Fredrickson <jeff@ejeff.org>
2024-03-24 18:27:00 -0600

    fix permissions after build

--- build.sh
+++ build.sh
@@ -3,9 +3,14 @@
 PLATFORM="linux"
 ARCH="x64"
 NAME="Fastmail"
+DIR="${NAME}-${PLATFORM}-${ARCH}"
 
 nativefier https://www.fastmail.com/mail/ --name ${NAME} --inject fastmail.css
 
 # Change package name from 'fastmail-nativefier-XXXXXX' to 'Fastmail'
-sed -i -E "s/fastmail-nativefier-.{6}/${NAME}/" ${NAME}-${PLATFORM}-${ARCH}/resources/app/package.json
+sed -i -E "s/fastmail-nativefier-.{6}/${NAME}/" ${DIR}/resources/app/package.json
+
+# Fix permissions
+find ${DIR} -type f -exec chmod go+r {} \;
+find ${DIR} -type d -exec chmod go+rx {} \;
 
